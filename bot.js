d4962f0107d7245d070a86d26cce3c8803f064d8
joao victor <joaovictor1c2013@gmail.com>
2020-03-31 21:49:00 -0300

    criacao do downloa de png

--- bot.js
+++ bot.js
@@ -4,8 +4,10 @@ var assert = require("assert"),
 var path = require("chromedriver").path;
 const fs = require("fs");
 const { By, until } = require("selenium-webdriver");
-
-var timeout = 10000;
+var atob = require("atob");
+const { extname, resolve } = require("path");
+var base64Img = require("base64-img");
+var timeout = 3000;
 
 async function GloboLogin() {
   var driver = chrome.Driver.createSession(
@@ -40,21 +42,43 @@ async function GloboLogin() {
   );
   var [, vote] = await driver.wait(function() {
     return driver.wait(
-      until.elementsLocated(By.className("_2VB0COy4AGiYeUkFpM5Oam")),
+      until.elementsLocated(By.className("_2ICyLVqxrk-FasJQIpaNzv")),
       timeout
     );
   });
   await vote.click(vote);
 
-  // var img = await driver.wait(function() {
-  //   return driver.wait(
-  //     until.elementsLocated(By.className("gc__3_EfD")),
-  //     timeout
-  //   );
-  // });
-  // console.log(img);
-  // const img2 = await img.getAttribute("src");
-  // console.log(img2);
+  var [img] = await driver.wait(function() {
+    return driver.wait(
+      until.elementsLocated(By.className("gc__3_EfD")),
+      timeout
+    );
+  });
+
+  var img2 = await img.getAttribute("src");
+
+  try {
+    let base64Image = img2.split(";base64,").pop();
+    var teste = await fs.writeFile(
+      `image.png`,
+      base64Image,
+      { encoding: "base64" },
+      function(err) {
+        console.log("File created");
+      }
+    );
+  } catch (err) {}
+
+  try {
+    var reload = await driver.wait(function() {
+      return driver.wait(
+        until.elementLocated(By.className("gc__1JSqe")),
+        timeout
+      );
+    });
+
+    await reload.click();
+  } catch (err) {}
 }
 
 GloboLogin();
